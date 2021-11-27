e26a674df11c071c34540bfcf3ec56c2424ebf77
mburakbilgic <53704957+mburakbilgic@users.noreply.github.com>
2021-11-28 00:22:17 +0300

    feat(basket-iter):basket summary ui v1, items now iterable

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 
+from store.models import Product
 
 
 class Basket():
@@ -14,11 +16,39 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, qty):
         """
         Adding and updating the users basket session data
         """
-        product_id = product.id
+        product_id = str(product.id)
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': product.price}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+
+        self.session.modified = True
+
+    def __iter__(self):
+        """
+        Collect the product-id in the session data to query the database
+        and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum
--- basket/views.py
+++ basket/views.py
@@ -1,16 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.csrf import csrf_exempt
 
 from store.models import Product
 
 from .basket import Basket
 
+
 def basket_summary(request):
     basket = Basket(request)
     return render(request, 'store/basket/summary.html', {'basket': basket})
 
+
+@csrf_exempt
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+
+        return response
